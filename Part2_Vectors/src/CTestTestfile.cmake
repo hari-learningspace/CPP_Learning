84a6d8d21f21eeaf5ee335d49857ed5be1885ff2
Hari Udayakumar <hariudkmr@gmail.com>
2020-05-17 19:39:50 +0800

    removed redudant cmake files

--- Part2_Vectors/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hudkmr/Udacity/CPP_Learning/Part2_Vectors/src
-# Build directory: /home/hudkmr/Udacity/CPP_Learning/Part2_Vectors/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
